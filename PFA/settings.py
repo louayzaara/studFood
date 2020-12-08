b30fd5462cd65dd6013a137ce79c74f2058aecc4
Firas Jebari <jbari.firas@protonmail.com>
2020-12-08 23:01:22 +0100

    whitenoise & gunicorn added

--- PFA/settings.py
+++ PFA/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -167,3 +168,4 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'PFA/media')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
